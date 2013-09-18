51189f3eed5f48fad268823343290dd44f47823c
Boris Filipov <obiwonn@gmail.com>
2013-09-18 14:21:03 +0300

    added expressSend

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var rest = require("organic-reactions-rest")
-var jsonResponse = require("organic-alchemy").http.jsonResponse;
+var chain = require("organic-alchemy").http.chain
+var fromActions = require("organic-reactions-rest").fromActions
+var expressSend = require("organic-reactions-expressSend").expressSend
 
-module.exports.crud = function(base_url, model) {
-  return rest.fromActions(base_url, {
-    "GET /list": function(req, res, next) {
-      model.find({}).exec(jsonResponse(res, next))
+module.exports.crud = function(base_url, model, sendReaction) {
+  return chain(fromActions(base_url, {
+    "GET /list":  function(req, res, next){
+      model.find({}).exec(next)
     },
     "GET /:id": function(req, res, next) {
-      model.findById(req.params.id).exec(jsonResponse(res, next))
+      model.findById(req.params.id).exec(next)
     },
     "POST /create": function(req, res, next) {
-      model.create(req.body, jsonResponse(res, next))
+      model.create(req.body, next)
     },
     "PUT /:id": function(req, res, next){
-      model.findByIdAndUpdate(req.params.id, req.body, jsonResponse(res, next))
+      model.findByIdAndUpdate(req.params.id, req.body, next)
     },
     "DELETE /:id": function(req, res, next) {
-      model.findByIdAndRemove(req.params.id, jsonResponse(res, next))
+      model.findByIdAndRemove(req.params.id, next)
     }
-  })
+  }), sendReaction || expressSend)
 }
